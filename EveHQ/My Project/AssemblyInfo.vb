5d2adce6d42f39137a8fe266b72ea960bd796bb6
Slivo-fr <gchopelet@gmail.com>
2015-02-18 22:33:26 +0100

    Svipul shield bonus

--- EveHQ/My Project/AssemblyInfo.vb
+++ EveHQ/My Project/AssemblyInfo.vb
@@ -13,7 +13,7 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyCompany("EveHQ team")> 
 <Assembly: AssemblyProduct("EveHQ")> 
 <Assembly: AssemblyCopyright("")> 
-<Assembly: AssemblyTrademark("2015-02-18")> 
+<Assembly: AssemblyTrademark("2015-02-19")> 
 
 <Assembly: ComVisible(False)> 
 
@@ -30,5 +30,5 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
 
-<Assembly: AssemblyVersion("2.17.0.0")> 
-<Assembly: AssemblyFileVersion("2.17.0.0")> 
+<Assembly: AssemblyVersion("2.17.1.0")> 
+<Assembly: AssemblyFileVersion("2.17.1.0")> 
