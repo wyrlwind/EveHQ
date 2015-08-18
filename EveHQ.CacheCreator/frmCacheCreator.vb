d63fd8a903593745f899a19260d3d0eee2fc10c8
Slivo <gchopelet@gmail.com>
2015-08-18 17:01:57 +0200

    WH lookup widget exception

--- EveHQ.CacheCreator/frmCacheCreator.vb
+++ EveHQ.CacheCreator/frmCacheCreator.vb
@@ -375,7 +375,9 @@ Public Class FrmCacheCreator
 
         StaticData.Types.Clear()
         Dim evehqData As DataSet
-        Dim strSql As String = "SELECT invTypes.*, invGroups.categoryID FROM invGroups INNER JOIN invTypes ON invGroups.groupID = invTypes.groupID where invTypes.published = 1;"
+        Dim strSql As String = ""
+        strSql &= "SELECT invTypes.*, invGroups.categoryID FROM invGroups INNER JOIN invTypes ON invGroups.groupID = invTypes.groupID "
+        strSql &= "where invTypes.published = 1 or invTypes.groupID = 988;" '988 is the group of 'wormholes gates'
         evehqData = DatabaseFunctions.GetStaticData(strSql)
         Dim newItem As EveType
         For Each itemRow As DataRow In evehqData.Tables(0).Rows
