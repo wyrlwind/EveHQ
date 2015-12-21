8728c04d0335c2d7e16229c8c197db1edcf83f26
bonekill <boneki11@hotmail.com>
2015-12-21 10:40:46 -0700

    Altered UI layout.

--- EveHQSharp/Windows/MainWindow.xaml.cs
+++ EveHQSharp/Windows/MainWindow.xaml.cs
@@ -23,64 +23,18 @@ namespace EveHQSharp
         public MainWindow()
         {
             InitializeComponent();
-            fitterTab.Visibility = Visibility.Collapsed;
-            industryTab.Visibility = Visibility.Collapsed;
-            pilotManagerTab.Visibility = Visibility.Collapsed;
+            EveHQSharp.UserControls.CoreUserControl thing = new EveHQSharp.UserControls.CoreUserControl();
+            coreGrid.Children.Add(thing);
 
         }
-
-        private void settingsButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (!Core.Classes.WindowStates.settingsWindowOpen)
-            {
-                Settings settingsWindow = new Settings();
-                settingsWindow.Show();
-            }
-        }
-
         private void Window_Closed(object sender, EventArgs e)
         {
             Environment.Exit(1);
         }
 
-        private void fitterButton_Click(object sender, RoutedEventArgs e)
+        private void tabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            if (fitterTab.Visibility != Visibility.Visible)
-            {
-                fitterTab.Visibility = Visibility.Visible;
-                Fitting.FittingUserControl fittingWindow = new EveHQSharp.Fitting.FittingUserControl();
-                fitterGrid.Children.Add(fittingWindow);
-            }
-        }
 
-        private void pilotManagerButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (pilotManagerTab.Visibility != Visibility.Visible)
-            {
-                pilotManagerTab.Visibility = Visibility.Visible;
-                PilotManager.UserControls.PilotManagerUserControl skillManagerWindow = new PilotManager.UserControls.PilotManagerUserControl();
-                pilotManagerGrid.Children.Add(skillManagerWindow);
-            }
-        }
-
-        private void industryButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (industryTab.Visibility != Visibility.Visible)
-            {
-                industryTab.Visibility = Visibility.Visible;
-                Industry.UserControls.IndustryUserControl industryWindow = new Industry.UserControls.IndustryUserControl();
-                industryGrid.Children.Add(industryWindow);
-            }
-
-        }
-
-        private void manageAPIButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (!Core.Classes.WindowStates.manageAPIWindowOpen)
-            {
-                EveAPI.Windows.ManageAPI manageAPIWindow = new EveAPI.Windows.ManageAPI();
-                manageAPIWindow.Show();
-            }
         }
     }
 }
